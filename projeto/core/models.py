b3b268930636245bfa4f4bf882287ade28b194ac
xJohn <jhfs632@gmail.com>
2022-09-12 22:16:50 -0300

    Revert "primeiro model TimeStampedModel"
    This reverts commit cd2c552a0cf3f01df5c2498ffbbeeb8d1a9ebc31.

--- projeto/core/models.py
+++ projeto/core/models.py
@@ -1,18 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class TimeStampedModel(models.Model):
-    created = models.DateTimeField(
-        'criado em',
-        auto_now=False,
-        auto_now_add=True,
-        )
-    modified = models.DateTimeField(
-        'modificado em',
-        auto_now= True,
-        auto_now_add=False,
-    )
-
-    class Meta:
-        abstract = True
