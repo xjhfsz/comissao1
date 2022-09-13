33fb94d63b3a3ed4a3c15711b5aa6ce0751b475a
xJohn <jhfs632@gmail.com>
2022-09-12 23:09:45 -0300

    add model Estoque + migrate

--- projeto/estoque/models.py
+++ projeto/estoque/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from projeto.core.models import TimeStampedModel
+from projeto.produto.models import Produto
 
-# Create your models here.
+MOVIMENTO = (
+    ('e', 'entrada'),
+    ('s', 'saída')
+)
+
+class Estoque(TimeStampedModel):
+    funcionario = models.ForeignKey(User, on_delete=models.CASCADE)
+    nf = models.PositiveIntegerField('Nota Fiscal', null=True, blank=True)
+    movimento = models.CharField(max_length=1, choices=MOVIMENTO)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return str(self.pk)
+
+class EstoqueItens(models.Model):
+    estoque = models.ForeignKey(Estoque, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.PositiveIntegerField()
+    saldo = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ('pk',)
+
+    def __str__(self):
+        return f'{self.pk} - {self.estoque.pk} - {self.produto}'
