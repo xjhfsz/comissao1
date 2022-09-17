8cdd34ce3d6ef2de153f2dfceb5b86741e7acb27
xjhfsz <jhfs632@gmail.com>
2022-09-17 09:03:48 -0300

    fix model e template
    correção da exibição do formato de data (strftime) e template - erro no chamamento da variável da lista de entradas

--- projeto/estoque/models.py
+++ projeto/estoque/models.py
@@ -17,8 +17,9 @@ class Estoque(TimeStampedModel):
         ordering = ('-created',)
 
     def __str__(self):
-        return '{} - {} - {}'.format(self.pk, self.nf, self.created.strftime('%d-%m-%Y'))
-        # return f'{self.pk} - {self.nf} - {self.created.strftime():%d/%m/%Y}'
+        # return '{} - {} - {}'.format(self.pk, self.nf, self.created.strftime('%d-%m-%Y'))
+        time = self.created.strftime('%d/%m/%Y')
+        return f'{self.pk} - {self.nf} - {time}'
 
     def nf_formated(self):
         return str(self.nf).zfill(4)
