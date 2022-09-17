697d3f2a0f61a3e611384e573008c4907c397d17
xjhfsz <jhfs632@gmail.com>
2022-09-17 09:55:07 -0300

    estoque entrada detail
    template do registro de entrada de estoque + fixes

--- projeto/estoque/models.py
+++ projeto/estoque/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse_lazy
 from django.contrib.auth.models import User
 from projeto.core.models import TimeStampedModel
 from projeto.produto.models import Produto
@@ -17,15 +18,21 @@ class Estoque(TimeStampedModel):
         ordering = ('-created',)
 
     def __str__(self):
-        # return '{} - {} - {}'.format(self.pk, self.nf, self.created.strftime('%d-%m-%Y'))
         time = self.created.strftime('%d/%m/%Y')
         return f'{self.pk} - {self.nf} - {time}'
+
+    def get_absolute_url(self):
+        return reverse_lazy('estoque:estoque_entrada_detail', kwargs={'pk': self.pk})
 
     def nf_formated(self):
         return str(self.nf).zfill(4)
 
 class EstoqueItens(models.Model):
-    estoque = models.ForeignKey(Estoque, on_delete=models.CASCADE)
+    estoque = models.ForeignKey(
+        Estoque,
+        on_delete=models.CASCADE,
+        related_name='estoques'
+    )
     produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
     quantidade = models.PositiveIntegerField()
     saldo = models.PositiveIntegerField()
