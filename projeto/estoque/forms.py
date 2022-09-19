e22224c5b0ecdebb5437daeb090c4eb527338b24
xjhfsz <jhfs632@gmail.com>
2022-09-19 18:28:02 -0300

    Update forms.py

--- projeto/estoque/forms.py
+++ projeto/estoque/forms.py
@@ -3,14 +3,12 @@ from .models import Estoque, EstoqueItens
 
 
 class EstoqueForm(forms.ModelForm):
-
     class Meta:
         model = Estoque
         fields = '__all__'
 
 
 class EstoqueItensForm(forms.ModelForm):
-
     class Meta:
         model = EstoqueItens
         fields = '__all__'
